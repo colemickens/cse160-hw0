d992427f2867b838aa8373f059617a6a2a1f9deb
Cole Mickens <cole.mickens@gmail.com>
2020-09-30 16:00:14 -0700

    initial commit

---
+++ hw0/main.py
@@ -0,0 +1,3 @@
+# Replace XXX with your answer to Part 2
+print("first is XXX")
+print("second is XXX")
---
+++ hw0/variables.py
@@ -0,0 +1,6 @@
+# Assign the value 160 into a variable
+
+# Multiple the value in the variable by 2 and
+# then store it back into the same variable
+
+# print out the variable
